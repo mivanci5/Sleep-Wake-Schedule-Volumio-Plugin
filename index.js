c09969ead3c7154a736407b118b4d084f2e790b6
mivanci5 <mario.ivancic@outlook.com>
2024-11-11 01:08:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ SleepWakePlugin.prototype.onVolumioStart = function () {
   this.writeLog('Plugin starting...');
 
   const configFile = this.commandRouter.pluginManager.getConfigurationFile(this.context, 'config.json');
+  this.config = new (require('v-conf'))();
   this.config.loadFile(configFile);
 
   return libQ.resolve();
